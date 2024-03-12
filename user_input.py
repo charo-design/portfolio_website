b4e29e289038441f1d46c27a6dd9f30bb204f39a
Ann Charity Mumbi Karoki <114146953+charo-design@users.noreply.github.com>
2024-03-12 21:16:44 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name:")
-age = input("Enter your current age:")
-location = input("Enter your current location:")
-print(f"Hey {name},you are {age} years old and stay at {location}")
